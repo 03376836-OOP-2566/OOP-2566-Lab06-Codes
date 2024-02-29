1ec21dc14c4a549b0593040963d6c89e5dd00721
Koson Trachu <koson.trachu@gmail.com>
2024-02-29 11:35:55 +0700

    lab 6 ex 3

---
+++ Lab06_Ex03/Program.cs
@@ -0,0 +1,17 @@
+﻿var student = new Student();
+student.name = "Enter student's name";
+System.Console.WriteLine(student.GetName());
+
+class Person
+{
+    public  string? name;
+    public string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+
+}
